4e28a8c8254ec421cdb6b4466090ecbd829d2eb3
Nikolos123 <Isponec2>
2022-03-22 00:15:41 +0300

    lesson_3 test files

--- library/authors/serializers.py
+++ library/authors/serializers.py
@@ -1,13 +1,33 @@
 import datetime
 
 from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer,HyperlinkedModelSerializer
 
 
-from .models import Author
+from .models import Author, Biography, Book
 
-class AuthorModelSerializer(serializers.ModelSerializer):
+
+class AuthorModelSerializer(ModelSerializer):
 
     class Meta:
         model = Author
-        fields = ('first_name','last_name')
+        fields = '__all__'
+        # exclude = ()
         # fields = ('first_name','las',)
+
+
+
+class BiographyModelSerializer(ModelSerializer):
+    # author = AuthorModelSerializer()
+    class Meta:
+        model = Biography
+        fields = '__all__'
+
+
+class BookModelSerializer(ModelSerializer):
+
+    # authors = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Book
+        fields = '__all__'
